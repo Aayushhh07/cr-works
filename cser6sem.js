e8ad3b60b26107ca8d02ed39d22189a736ba6ebf
Aayush Vishwakarma <aayushvishwakarma93@gmail.com>
2025-02-17 22:11:02 +0530

    added 6th sem file

---
+++ cser6sem.js
@@ -0,0 +1,198 @@
+
+let monday = [
+    {
+Subject_Name : "Computer Network <br> (ICS 603)",
+Time_Start: "10:00 AM",
+Time_End: "11:40 AM",
+Faculty_Name: "Mr. Abhishek Singh",
+Venue: "LT 26"
+    },
+    {
+Subject_Name : "Open Elective <br>(ICS 06*)",
+Time_Start: "11:40 AM",
+Time_End: "12:30 PM",
+Faculty_Name: "-",
+Venue: "LT 26"
+    },
+    {
+        Subject_Name : "<b>Section 1:</b> <br> Software Engineering <br>Lab",
+        Time_Start: "02:00 PM",
+        Time_End: "03:40 PM",
+        Faculty_Name: "Ms Srishti Tiwari",
+        Venue: "OS LAB"
+            }
+            ,
+            {
+                
+        Subject_Name : "<b>Section 2:</b> <br> Computer <br> Network  Lab",
+        Time_Start: "02:00 PM",
+        Time_End: "03:40 PM",
+        Faculty_Name: "Mr. Abhishek Singh",
+        Venue: "DBMS Lab"
+            }
+            ,
+    {
+Subject_Name : "Software Engineering <br> (ICS 601)",
+Time_Start: "03:40 PM",
+Time_End: "04:30 PM",
+Faculty_Name: "Ms Srishti Tiwari",
+Venue: "LT 26"
+    }
+
+]
+let tuesday = [
+    {
+Subject_Name : "Software Engineering<br>(ICS 601)",
+Time_Start: "09:10 AM",
+Time_End: "10:50 AM",
+Faculty_Name: "Ms Srishti Tiwari",
+Venue: "LT 26"
+    },
+    {
+Subject_Name : "Open Elective<br>(ICS 06*)",
+Time_Start: "10:50 AM",
+Time_End: "12:30 PM",
+Faculty_Name: "-",
+Venue: "LT 26"
+    },
+    {
+        
+Subject_Name : "<b>Section 1:</b> <br> Artificial Intelligence <br>Lab",
+Time_Start: "02:00 PM",
+Time_End: "03:40 AM",
+Faculty_Name: "Dr. Promila Bahadur",
+Venue: "DBMS LAB"
+    },
+
+    {
+        
+Subject_Name : "<b>Section 2:</b> <br> Software Engineering<br>Lab",
+Time_Start: "02:00 PM",
+Time_End: "03:40 PM",
+Faculty_Name: "Ms Srishti Tiwari",
+Venue: "OS Lab"
+    }
+    ,
+    {
+        Subject_Name : "Artificial Intelligence <br>(ICS 602)",
+        Time_Start: "03:40 PM",
+        Time_End: "04:30 PM",
+        Faculty_Name: "Dr. Promila Bahadur",
+        Venue: "LT 26"
+            }
+    
+];
+let wed = [
+    {
+        
+        Subject_Name : "<b> Minor Degree </b>",
+        Time_Start: "09:00 AM",
+        Time_End: "10:50 AM",
+        Faculty_Name: "-",
+        Venue: "-"
+            },
+    
+    {
+Subject_Name : "Artificial Intelligence <br> (ICS 602)",
+Time_Start: "10:50 AM",
+Time_End: "12:30 PM",
+Faculty_Name: "Dr. Promila Bahadur",
+Venue: "LT 26"
+    },
+
+{
+Subject_Name : "Application of <br> Soft Computing <br> (ICS 064)",
+Time_Start: "02:00 PM",
+Time_End: "02:50 PM",
+Faculty_Name: "Dr. Pawan Kumar Tiwari",
+Venue: "LT 26"
+    },
+
+{Subject_Name : "<b>Section 2:</b> <br> Seminar/ Startup <br> (ICS 654)",
+Time_Start: "02:50 PM",
+Time_End: "04:30 PM",
+Faculty_Name: "Dr. Pawan Kumar Tiwari",
+Venue: "LT 26"
+    }
+
+]
+
+
+let thus = [
+    {
+        Subject_Name : "Software Engineering<br>(ICS 601)",
+        Time_Start: "09:10 AM",
+        Time_End: "10:50 AM",
+        Faculty_Name: "Ms Srishti Tiwari",
+        Venue: "LT 26"
+            },
+
+            {
+                Subject_Name : "Application of <br> Soft Computing <br> (ICS 064)",
+                Time_Start: "10:50 PM",
+                Time_End: "12:30 PM",
+                Faculty_Name: "Dr. Pawan Kumar Tiwari",
+                Venue: "LT 26"
+                    },
+                    {
+                
+                        Subject_Name : "<b>Section 1:</b> <br> Computer <br> Network  Lab",
+                        Time_Start: "02:00 PM",
+                        Time_End: "03:40 PM",
+                        Faculty_Name: "Mr. Abhishek Singh",
+                        Venue: "DBMS Lab"
+                            }
+                            ,
+    
+                    {
+        
+                        Subject_Name : "<b>Section 2:</b> <br> Artificial Intelligence <br>Lab",
+                        Time_Start: "02:00 PM",
+                        Time_End: "03:40 AM",
+                        Faculty_Name: "Dr. Promila Bahadur",
+                        Venue: "DBMS LAB"
+                            }
+]
+let fri = [
+    {
+        Subject_Name : "Computer Network <br> (ICS 603)",
+        Time_Start: "10:00 AM",
+        Time_End: "10:50 AM",
+        Faculty_Name: "Mr. Abhishek Singh",
+        Venue: "LT 26"
+            },
+        {
+            Subject_Name : "Artificial Intelligence <br> (ICS 602)",
+            Time_Start: "10:50 AM",
+            Time_End: "12:30 PM",
+            Faculty_Name: "Dr. Promila Bahadur",
+            Venue: "LT 26"
+                },
+
+             {Subject_Name : "<b>Section 2:</b> <br> Seminar/ Startup <br> (ICS 654)",
+            Time_Start: "02:00 PM",
+            Time_End: "03:40 PM",
+            Faculty_Name: "Dr. Pawan Kumar Tiwari",
+            Venue: "LT 26"
+             }
+                    
+]
+let sat = [
+    {
+        Subject_Name : "Computer Network <br> (ICS 603)",
+        Time_Start: "10:50 AM",
+        Time_End: "12:30 AM",
+        Faculty_Name: "Mr. Abhishek Singh",
+        Venue: "LT 26"
+            },
+    {
+        
+        Subject_Name : "Minor Degree",
+        Time_Start: "02:00 PM",
+        Time_End: "03:40 PM",
+        Faculty_Name: "-",
+        Venue: "-"
+            }
+    
+
+]
